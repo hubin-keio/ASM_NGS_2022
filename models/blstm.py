0ad1724b16f227a4e9906c775d038e44ff95d974
Bin HU <8225406+hubin-keio@users.noreply.github.com>
2022-09-25 17:35:41 -0600

    added model parameter summary and moved data shuffle inside epoch

--- models/blstm.py
+++ models/blstm.py
@@ -13,6 +13,7 @@ from torch import nn
 from torch.utils.data import Dataset
 from torch.utils.data import DataLoader
 from torch.utils.data import random_split
+from prettytable import PrettyTable
 
 class Transformer:
     """Transforms input sequence to features"""
@@ -218,16 +219,16 @@ class BLSTM(nn.Module):
 
 
 def run_lstm(model: BLSTM,
-             train_loader: DataLoader,
-             test_loader: DataLoader,
+             train_set: Dataset,
+             test_set: Dataset,
              n_epochs: int,
              batch_size: int,
              device: str):
     """Run LSTM model
 
     model: BLSTM,
-    train_loader: training set dataLoader,
-    test_loader: test det dataLoader,
+    train_set: training set dataset
+    test_set: test det dataset
     n_epochs: number of epochs
     batch_size: batch size
     device: 'gpu' or 'cpu'
@@ -235,6 +236,8 @@ def run_lstm(model: BLSTM,
 
     L_RATE = 1e-5               # learning rate
     model = model.to(device)
+
+
 
     loss_fn = nn.MSELoss(reduction='sum').to(device)  # MSE loss with sum
     optimizer = torch.optim.SGD(model.parameters(), L_RATE)  # SGD optimizer
@@ -244,6 +247,10 @@ def run_lstm(model: BLSTM,
     for epoch in range(1, n_epochs + 1):
         train_loss = 0
         test_loss = 0
+
+        train_loader = DataLoader(train_set, batch_size=BATCH_SIZE, shuffle=True)
+        test_loader = DataLoader(test_set, batch_size=BATCH_SIZE, shuffle=True)
+
 
         for batch, (label, feature, target) in enumerate(train_loader):
             optimizer.zero_grad()
@@ -300,22 +307,27 @@ def plot_history(train_losses: list, n_train: int, test_losses: list,
     fig.savefig(save_as + '.png')
     history_df.to_csv(save_as + '.csv')
 
-
-
+def count_parameters(model):
+    table = PrettyTable(["Modules", "Parameters"])
+    total_params = 0
+    for name, parameter in model.named_parameters():
+        if not parameter.requires_grad: continue
+        params = parameter.numel()
+        table.add_row([name, params])
+        total_params+=params
+    print(table)
+    print(f"Total Trainable Params: {total_params}")
+    return total_params
 
 if __name__=='__main__':
-
     ROOT_DIR = os.path.join(os.path.dirname(__file__), '..')
     ROOT_DIR = os.path.abspath(ROOT_DIR)
     DATA_DIR = os.path.join(ROOT_DIR, 'datasets')
 
 
-    KD_CSV = os.path.join(DATA_DIR, 'mutation_binding_Kds.csv')
+    KD_CSV = os.path.join(DATA_DIR, 'mutation_binding_Kds_head_1000.csv')
     REFSEQ = 'NITNLCPFGEVFNATRFASVYAWNRKRISNCVADYSVLYNSASFSTFKCYGVSPTKLNDLCFTNVYADSFVIRGDEVRQIAPGQTGKIADYNYKLPDDFTGCVIAWNSNNLDSKVGGNYNYLYRLFRKSNLKPFERDISTEIYQAGSTPCNGVEGFNCYFPLQSYGFQPTNGVGYQPYRVVVLSFELLHAPATVCGPKKST'
     GLOVE_CSV = os.path.join(ROOT_DIR, 'glove/glove_dms_k6_s2.vector.txt')
-
-
-
 
     # Run setup
     DEVICE = 'cuda' if torch.cuda.is_available else 'cpu'
@@ -325,7 +337,7 @@ if __name__=='__main__':
     LSTM_HIDDEN_SIZE = 50       # lstm_hidden_size
     LSTM_NUM_LAYERS = 1         # lstm_num_layers
     LSTM_BIDIRECTIONAL = True   # lstm_bidrectional
-    FCN_HIDDEN_SIZE = 20        # fcn_hidden_size
+    FCN_HIDDEN_SIZE = 100        # fcn_hidden_size
 
     # Dataset split and dataloader
     glove_transformer = Transformer('glove_kmer', glove_csv=GLOVE_CSV)
@@ -334,10 +346,6 @@ if __name__=='__main__':
     TEST_SIZE = len(data_set) - TRAIN_SIZE
     train_set, test_set = random_split(data_set, (TRAIN_SIZE, TEST_SIZE))
 
-    train_loader = DataLoader(train_set, batch_size=BATCH_SIZE, shuffle=True)
-    test_loader = DataLoader(test_set, batch_size=BATCH_SIZE, shuffle=True)
-    labels, train_features , train_targets = next(iter(train_loader))
-
     model = BLSTM(BATCH_SIZE,
                   LSTM_INPUT_SIZE,
                   LSTM_HIDDEN_SIZE,
@@ -345,7 +353,9 @@ if __name__=='__main__':
                   LSTM_BIDIRECTIONAL,
                   FCN_HIDDEN_SIZE,
                   DEVICE)
-    train_losses, test_losses = run_lstm(model, train_loader, test_loader, N_EPOCHS, BATCH_SIZE, DEVICE)
+
+    count_parameters(model)
+    train_losses, test_losses = run_lstm(model, train_set, test_set, N_EPOCHS, BATCH_SIZE, DEVICE)
     model_result = f'blstm_train_{TRAIN_SIZE}_test_{TEST_SIZE}_{date.today()}'
     model_result = os.path.join(ROOT_DIR, f'plots/{model_result}')
     plot_history(train_losses, TRAIN_SIZE, test_losses, TEST_SIZE, model_result)
